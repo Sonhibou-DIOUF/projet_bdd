33d11032c80afa357191a9a8c794245a5578524e
safae-lbl <labelsafae@gmail.com>
2024-12-09 13:56:23 +0100

    Update logout.php

--- public/login/logout.php
+++ public/login/logout.php
@@ -1,21 +1,10 @@
-
 <?php
+// Démarre une nouvelle session ou reprend une session existante
 session_start();
-    $_SESSION = [];
+
+// Vide toutes les variables de session pour déconnecter l'utilisateur
+$_SESSION = [];
+
+// Redirige l'utilisateur vers la page d'accueil
 header('location: ../index.php');
-
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
