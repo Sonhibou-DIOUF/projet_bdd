b8b58ab0dcb832f92b6a774ef09a9e30289408fb
safae-lbl <labelsafae@gmail.com>
2024-12-09 15:15:25 +0100

    Update alert.php

--- public/composants/alert.php
+++ public/composants/alert.php
@@ -1,22 +1,26 @@
 <?php
+// Vérifie si une session 'message' existe et l'affiche comme une alerte bootstrap
 if (isset($_SESSION['message'])) {
     echo '<div class="alert alert-primary alert-dismissible fade show" role="alert">'
     . $_SESSION["message"] .
-  '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>';
 }
+
+// Vérifie si une session 'error' existe et l'affiche comme une alerte bootstrap
 if (isset($_SESSION['error'])) {
     echo
     '<div class="alert alert-danger alert-dismissible fade show" role="alert">
     . $_SESSION["error"] .
-  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>';
 }
+
+// Vérifie si une session 'success' existe et l'affiche comme une alerte bootstrap
 if (isset($_SESSION['success'])) {
     echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
     . $_SESSION["success"] .
-  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>';
 }
 ?>
-
