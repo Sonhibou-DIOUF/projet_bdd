1042e9d1c0c85d01fef161a3ec61848ad8327720
safae-lbl <labelsafae@gmail.com>
2024-12-09 14:19:27 +0100

    Update delete_photo.php

--- public/pages/dashboard/delete_photo.php
+++ public/pages/dashboard/delete_photo.php
@@ -6,15 +6,18 @@ include "../../login/connexion_bdd.php"; // Connexion à la base de données
 if (isset($_GET['id'])) {
     $id_photo = $_GET['id'];
 
-    // Requête SQL pour supprimer la séance
+    // Requête SQL pour supprimer la photo
     $sql = "DELETE FROM Photo WHERE id_photo = '$id_photo'";
 
+    // Exécuter la requête SQL et vérifier si elle a réussi
     if (mysqli_query($conn, $sql)) {
+        // Si la suppression a réussi, afficher une alerte et rediriger vers la page des photos
         echo "<script>
             alert('Photo supprimée avec succès.');
             window.location.href = 'photos.php'; // Redirection vers la page des photos
         </script>";
     } else {
+        // Si la suppression a échoué, afficher une alerte d'erreur et rediriger vers la page des photos
         echo "<script>
             alert('Erreur lors de la suppression de la photo.');
             window.location.href = 'photos.php';
@@ -26,6 +29,7 @@ if (isset($_GET['id'])) {
     exit();
 }
 
+// Fermer la connexion à la base de données
 mysqli_close($conn);
 
 ?>
