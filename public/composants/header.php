89c580a2498f1479636eb72c0fa279a10fd961e2
safae-lbl <labelsafae@gmail.com>
2024-12-09 15:22:49 +0100

    Update header.php

--- public/composants/header.php
+++ public/composants/header.php
@@ -5,13 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tableau de Bord</title>
-    <!-- Bootstrap CSS -->
+    <!-- Inclusion de Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- FontAwesome CSS -->
+    <!-- Inclusion de FontAwesome CSS pour les icônes -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-    <!-- Custom CSS -->
+    <!-- Inclusion de Custom CSS pour les styles personnalisés -->
     <link rel="stylesheet" href="../../../ressources/css/dashboard.css">
     <style>
+        /* Styles personnalisés pour la barre de navigation */
         .navbar-custom  {
             background-color:#02244b ;
             color: white;
@@ -29,9 +30,9 @@
             color: #ffffff;
         }
     </style>
+    <!-- Inclusion de Popper.js et Bootstrap JavaScript pour le fonctionnement des composants Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-
 </head>
 
 <body>
