605f5d253cdb7697acb2291e03231eb84b6a71fa
safae-lbl <labelsafae@gmail.com>
2024-12-09 14:17:45 +0100

    Update delete_client.php

--- public/pages/dashboard/delete_client.php
+++ public/pages/dashboard/delete_client.php
@@ -6,15 +6,18 @@ include "../../login/connexion_bdd.php"; // Connexion à la base de données
 if (isset($_GET['id'])) {
     $id_client = $_GET['id'];
 
-    // Requête SQL pour supprimer la séance
+    // Requête SQL pour supprimer le client
     $sql = "DELETE FROM Client WHERE id_client = '$id_client'";
 
+    // Exécuter la requête SQL et vérifier si elle a réussi
     if (mysqli_query($conn, $sql)) {
+        // Si la suppression a réussi, afficher une alerte et rediriger vers la page des clients
         echo "<script>
             alert('Client supprimé avec succès.');
             window.location.href = 'clients.php'; // Redirection vers la page des clients
         </script>";
     } else {
+        // Si la suppression a échoué, afficher une alerte d'erreur et rediriger vers la page des clients
         echo "<script>
             alert('Erreur lors de la suppression du client.');
             window.location.href = 'clients.php';
@@ -26,6 +29,7 @@ if (isset($_GET['id'])) {
     exit();
 }
 
+// Fermer la connexion à la base de données
 mysqli_close($conn);
 
 ?>
