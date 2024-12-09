770d030c9acfe9e882381d44a62769c9135baace
safae-lbl <labelsafae@gmail.com>
2024-12-09 13:53:28 +0100

    Update connexion_bdd.php

--- public/login/connexion_bdd.php
+++ public/login/connexion_bdd.php
@@ -1,17 +1,27 @@
 <?php
+// Démarre une nouvelle session ou reprend une session existante
 session_start();
+
+// Active l'affichage de tous les rapports d'erreurs
 error_reporting(E_ALL);
+// Configure PHP pour afficher les erreurs à l'écran
 ini_set('display_errors', 1);
 
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'PicturMe';
-//On établit la connexion
-$conn = mysqli_connect($servername, $username, $password,$database);
-// Verifier la connexion
-if($conn->connect_error){
-    die('Erreur : ' .$conn->connect_error);
+// Informations de connexion à la base de données
+$servername = 'mysql.ensinfo.sciences.univ-nantes.prive';
+$username = 'E249694G';
+$password = 'F02ARU6H';
+$database = 'E249694G';
+
+// Établit la connexion avec la base de données
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Vérifie si la connexion a échoué
+if ($conn->connect_error) {
+    // Arrête le script et affiche un message d'erreur
+    die('Erreur : ' . $conn->connect_error);
 }
+
+// Affiche un message de succès si la connexion est établie (désactivé)
 // echo 'Connexion réussie';
 ?>
