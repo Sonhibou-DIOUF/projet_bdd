305ca85aeb537e6aff1f5f7cbecf5008276dad06
Sonhibou DIOUF <hiboudiouf97@gmail.com>
2024-12-10 18:46:37 +0100

    modifs pour eviter les injection sql

--- public/composants/alert.php
+++ public/composants/alert.php
@@ -10,26 +10,19 @@ if (isset($_SESSION['message'])) {
 // Vérifie si une session 'error' existe et l'affiche comme une alerte bootstrap
 if (isset($_SESSION['error'])) {
     echo
-    '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    '<div class="alert alert-danger alert-dismissible fade show" role="alert">'
     . $_SESSION["error"] .
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>';
 }
 
 // Vérifie si une session 'success' existe et l'affiche comme une alerte bootstrap
 if (isset($_SESSION['success'])) {
-    echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+    echo '<div class="alert alert-success alert-dismissible fade show" role="alert">'
     . $_SESSION["success"] .
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>';
 }
-
-    // JavaScript pour supprimer le message après 1 seconde
-    echo "<script>
-        setTimeout(function() {
-            document.getElementById('message').style.display = 'none';
-        }, 1000); // 1000 millisecondes = 1 seconde
-    </script>";
 
     // Supprimer la variable de session après 1 seconde côté serveur
     unset($_SESSION['message']);
